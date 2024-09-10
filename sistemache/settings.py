62cf6fb384e879cbbee613f0dd50261d65e77dd8
IvanhoeG23R <ivanhoeg8@gmail.com>
2024-09-10 11:42:08 -0500

    Update settings.py

--- sistemache/settings.py
+++ sistemache/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = "/home/Ivanhoe/SistemaPedidos2024/static"
+STATIC_ROOT = "/home/kevin2001/SistemaPedidos2024/static"
 
 ROOT_URLCONF = 'sistemache.urls'
 
