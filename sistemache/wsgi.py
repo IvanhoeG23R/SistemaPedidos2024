2a2733d7421de58e60498670f1354b152b26df76
IvanhoeG23R <ivanhoeg8@gmail.com>
2024-09-10 10:51:26 -0500

    Update wsgi.py

--- sistemache/wsgi.py
+++ sistemache/wsgi.py
@@ -1,9 +1,14 @@
+"""
+WSGI config for sistemache project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
 import os
-import sys
-path = '/home/Ivanhoe/Sistemapedidos'
-if path not in sys.path:
-    sys.path.insert(0,path)
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'sistemache.settings'
+
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistemache.settings')
+
+application = get_wsgi_application()
